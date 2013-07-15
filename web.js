4b931271c8254fffeae938fc23f8cf8240226cd3
shekhjha <shekhjha@gmail.com>
2013-07-15 01:55:32 +0000

    Trying to read index.html from web.js

--- web.js
+++ web.js
@@ -2,19 +2,10 @@ fs = require('fs')
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var buffer = new Buffer("I'm a string!", "utf-8")
-var output =
-
-fs.readFileSync('index.html', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  buffer=data;
-});
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-  response.send(buffer.toString());
+  response.send(fs.readFileSync("index.html").toString());
 });
 
 var port = process.env.PORT || 5000;
